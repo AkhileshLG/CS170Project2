dab4104024b86630af5799f282a7ab58127b8538
AkhileshLG <akhileshgenneri@gmail.com>
2024-11-07 15:00:12 -0800

    added inputs for number of features and the chosen algorithm

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to Akhil, Sandeep, and Ethan's Feature Selection Algorithm.\n")
+numOfFeatures = int(input("Please enter total number of features: "))
+print("\n")
+
+print("Type the number of the algorithm you want to run.\n")
+print("1. Forward Selection\n2. Backward Elimination\nBertie's Special Algorithm\n")
+algorithmChoice = int(input())
+print("\n")
+
