cd860f22afd901f4e295d4ee998b31484f536090
AkhileshLG <akhileshgenneri@gmail.com>
2024-11-07 15:18:22 -0800

    program prints out accuracy without features and random evaluation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 print("Welcome to Akhil, Sandeep, and Ethan's Feature Selection Algorithm.\n")
 numOfFeatures = int(input("Please enter total number of features: "))
 print("\n")
@@ -6,6 +8,9 @@ print("Type the number of the algorithm you want to run.\n")
 print("1. Forward Selection\n2. Backward Elimination\nBertie's Special Algorithm\n")
 algorithmChoice = int(input())
 print("\n")
+
+randomVal = str(round(random.uniform(0, 100), 1))
+print("Using no features and \"randon\" evalution, I get an accuracy of " + randomVal + "%\n")
 
 if algorithmChoice == 1:
     print()
