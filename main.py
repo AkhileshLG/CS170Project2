7e89c17ce1376e255c804e8f26714784a7194bba
AkhileshLG <akhileshgenneri@gmail.com>
2024-11-19 14:09:41 -0800

    working on printing forwards selection

--- main.py
+++ main.py
@@ -13,10 +13,31 @@ print("\n")
 randomVal = str(round(random.uniform(50, 100), 1))
 print("Using no features and \"randon\" evaluation, I get an accuracy of " + randomVal + "%\n")
 
+maxAccuracy = str(0)
+featureLst = []
+result = []
+featureLstInput = 1
+maxValue = 1
+
+for i in numOfFeatures:
+    featureLst.append(featureLstInput)
+    featureLstInput += 1
+
 if algorithmChoice == 1:
-    pathPrint(numOfFeatures)
+    while len(featureLst) != 0:
+        for x in featureLst:
+            currentVal = str(x)
+            tempAccuracy = str(round(random.uniform(50, 100), 1))
+            print("Using feature(s) \{" + currentVal + "\} accuracy is " + tempAccuracy)
+
+            if maxAccuracy <= tempAccuracy:
+                maxAccuracy = tempAccuracy
+                maxValue = currentVal
+        
+        result.append(maxValue)
+        featureLst.remove(maxValue)
+            
 elif algorithmChoice == 2:
     print()
 elif algorithmChoice == 3:
     print()
-
