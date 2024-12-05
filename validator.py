f723ed5b62f47008566fa3e8278c857afbcdadc5
Ethan Lingad <ethan@Ethans-Air-2.lan>
2024-12-04 22:58:51 -0800

    validator

---
+++ validator.py
@@ -0,0 +1,33 @@
+from NNclassifier import NNClassifier
+import numpy as np
+class Validator:
+    def __init__(self,classifier);
+        self.classifier = classifier
+    #passing in feature data, correct label, and feature subsets
+    def evaluate(self, data, labels, featureSubset):
+        correctPredictions = 0
+        numInstances = len(data)
+
+        for i in range(numInstances):
+            #i gets the index of the instance and feature subset give
+            testInstance = []
+            for j in featureSubset:
+                testInstance.append(data[i][j])
+            testLabel = labels[i]
+            trainData = []
+            trainLabels = []
+
+            for j in range(len(data)):
+                if j != i:  
+                    trainData.append([data[j][k] for k in featureSubset])
+                    trainLabels.append(labels[j])
+            trainData = np.array(trainData)
+            trainLabels = np.array(trainLabels)
+            predictedLabel = self.classifier.train(trainData, trainLabels)
+
+            if(predictedLabel == testLabel):
+                correctPredictions+=1
+            
+        accuracy = correctPredictions / numInstances
+        return accuracy
+
